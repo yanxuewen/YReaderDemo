7a68f36093fa82342fa45f4ba7ee2eec984aaa11
yanxuewen <yanxuewen325@163.com>
2017-03-04 22:56:02 +0800

    add speech view

---
+++ YReaderDemo/YReaderDemo/Reader/Speech/YSpeechViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSpeechViewController.h
+//  YReaderDemo
+//
+//  Created by yanxuewen on 2017/3/4.
+//  Copyright © 2017年 yxw. All rights reserved.
+//
+
+#import "YBaseViewController.h"
+
+@interface YSpeechViewController : YBaseViewController
+
+@end
---
+++ YReaderDemo/YReaderDemo/Reader/Speech/YSpeechViewController.m
@@ -0,0 +1,37 @@
+//
+//  YSpeechViewController.m
+//  YReaderDemo
+//
+//  Created by yanxuewen on 2017/3/4.
+//  Copyright © 2017年 yxw. All rights reserved.
+//
+
+#import "YSpeechViewController.h"
+
+@interface YSpeechViewController ()
+
+@end
+
+@implementation YSpeechViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
